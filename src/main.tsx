a64ba0201de9dec4afc1a62d9b8fb63686bd7561
Meiriellyssn <merielly.ssousa@gmail.com>
2025-09-07 16:13:23 -0300

    criação de paginade de erro

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import 'react-calendar/dist/Calendar.css';
 
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
@@ -8,11 +9,13 @@ import { LoginPage } from './pages/Auth/LoginPage.tsx';
 import { AdminDashboard } from './pages/Dashboard/AdminDashboard.tsx';
 import { PatientDashboard } from './pages/Dashboard/PatientDashboard.tsx';
 import { ProfessionalDashboard } from './pages/Dashboard/ProfessionalDashboard.tsx';
+import { ErrorPage } from './pages/ErrorPage.tsx'; // <-- 1. Importe a página de erro
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <LoginPage />,
+    errorElement: <ErrorPage />,
   },
   {
     path: '/dashboard-admin',
